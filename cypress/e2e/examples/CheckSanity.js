24f9abd4d3edffebe6c517c676bc68e4cc9d89c7
Abhishek K M <abhishekabhi6823@gmail.com>
2026-01-30 14:45:31 +0530

    modified the CheckSanity.js

--- cypress/e2e/examples/CheckSanity.js
+++ cypress/e2e/examples/CheckSanity.js
@@ -1,6 +1,7 @@
+
 // This test is to check whether we have any issues from import, config files
 
-describe('Sanity Check', () => {
+describe('Checking the sanity', () => {
   it('should pass if the environment is clean', () => {
     expect(true).to.be.true;
   });
